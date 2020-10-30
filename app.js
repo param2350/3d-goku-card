cf953dc63604fa49c1affb927d425f0c81924b98
Paramvir Ramola <ramolaparamvir@gmail.com>
2020-10-30 16:44:31 +0530

    v2

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const goku = document.querySelector('.goku img');
 const kakarot = document.querySelector('.kakarot');
 const description = document.querySelector('.info h3');
 const powers = document.querySelector('.powers');
+const circle = document.querySelector('.circle');
+
 
 
 container.addEventListener('mousemove', (e) => {
@@ -14,6 +16,8 @@ container.addEventListener('mousemove', (e) => {
     let yAxis = (window.innerWidth / 2 - e.pageY) /25;
 
     card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+    
+
 
 });
 
@@ -23,12 +27,14 @@ container.addEventListener('mouseenter', e => {
     card.style.transition = 'none';
 
     
-    title.style.transform = "translateZ(55px)";
-    goku.style.transform = "translateZ(75px) translateY(-25px) rotatex(-20deg)";
+    title.style.transform = "translateZ(55px)";;
 
     powers.style.transform = "translateZ(50px)";
+    circle.style.transform = "translateY(35px) rotateX(40deg)";
+
 
     description.style.transform = "translateZ(50px)";
+    goku.style.transform = "translateZ(75px) translateY(-25px)  rotateX(-30deg)";
 
     kakarot.style.transform = "translateZ(50px)";
 
@@ -41,12 +47,17 @@ container.addEventListener('mouseleave', e => {
     
 
     title.style.transform = "translateZ(0px)";
-    goku.style.transform = "translateZ(0px) rotateZ(0deg)";
+    goku.style.transform = "translateZ(0) translateY(0)  rotateX(0)";
 
     powers.style.transform = "translateZ(0px)";
 
     description.style.transform = "translateZ(0px)";
 
     kakarot.style.transform = "translateZ(0px)";
+    circle.style.transform = "rotateX(0deg)";
+
+    circle.style.transform = "translateY(0px)";
+
+
 
 });
